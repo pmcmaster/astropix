2cb2038bbcd74e176714273077f40059af24fc77
Peter McMaster <e2rf6zsmmd@liamekaens.com>
2024-08-02 20:56:32 +0100

    Initial Commit

---
+++ AstroPix/AstroPixApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroPixApp.swift
+//  AstroPix
+//
+//  Created by Peter McMaster on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct AstroPixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
